b06dd7f701dcc3867f371167dd0405f28932c920
Sultan Tuzelov <uissless@gmail.com>
2020-02-22 13:33:56 +0600

    End of Section 4

--- API/Controllers/WeatherForecastController.cs
+++ API/Controllers/WeatherForecastController.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using Persistence;
-using Domain;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
